19e11e190c5361b249310a0e82c50645299fbfc9
Aleksandar Prokopec <aleksandar.prokopec@oracle.com>
2019-04-08 23:52:24 +0200

    Add the Twitter Finagle benchmark group.

---
+++ benchmarks/twitter-finagle/src/main/scala/org/renaissance/twitter/finagle/FinagleHttp.scala
@@ -0,0 +1,15 @@
+package org.renaissance.twitter.finagle
+
+import org.renaissance.Config
+import org.renaissance.License
+import org.renaissance.RenaissanceBenchmark
+
+class FinagleHttp extends RenaissanceBenchmark {
+  def description = "Sends many small Finagle HTTP requests and awaits the response."
+
+  override def defaultRepetitions = 12
+
+  def licenses = License.create(License.APL2)
+
+  override def runIteration(c: Config): Unit = {}
+}
