b63d4fc7b77d516c0535425040950909fb2f7bfb
Francois Farquet <francois.farquet@oracle.com>
2019-04-11 09:55:07 +0200

    typo

--- src/main/scala/org/renaissance/renaissance-suite.scala
+++ src/main/scala/org/renaissance/renaissance-suite.scala
@@ -421,7 +421,7 @@ and included into the Renaissance distribution.
 
 ### Benchmark evaluation and release process
 
-In the open-source spirit, anyone can propose an additional benchmark by creating pull request.
+In the open-source spirit, anyone can propose an additional benchmark by opening a pull request.
 The code is then inspected by the community -- typically, the suite maintainers are involved
 in the review, but anybody else is free join the discussion.
 During the discussion, the reviewers suggest the ways in which
