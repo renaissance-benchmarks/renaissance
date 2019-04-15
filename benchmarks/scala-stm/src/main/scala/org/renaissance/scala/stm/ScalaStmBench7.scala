1a71ed39c5da6a5419f52120b4be109ca2242a92
Aleksandar Prokopec <aleksandar.prokopec@oracle.com>
2019-04-15 13:39:45 +0200

    Add comments for the stm-bench7 parameters.

--- benchmarks/scala-stm/src/main/scala/org/renaissance/scala/stm/ScalaStmBench7.scala
+++ benchmarks/scala-stm/src/main/scala/org/renaissance/scala/stm/ScalaStmBench7.scala
@@ -15,6 +15,14 @@ class ScalaStmBench7 extends RenaissanceBenchmark {
   val THREAD_COUNT = Runtime.getRuntime.availableProcessors
 
   override def runIteration(c: Config): Unit = {
+    // TODO: Consolidate benchmark parameters across the suite.
+    //  See: https://github.com/D-iii-S/renaissance-benchmarks/issues/27
+    // The following is the description of STMBench7's arguments.
+    // -s -- the initializer class for the STM implementation
+    // -g -- the type of synchronization that the benchmark should use
+    // -w -- the type of the workload (we use the read/write workload)
+    // -c -- the count of operations to execute in the benchmark
+    // -t -- the number of threads to use
     val args = Array(
       "-s",
       "stmbench7.scalastm.ScalaSTMInitializer",
