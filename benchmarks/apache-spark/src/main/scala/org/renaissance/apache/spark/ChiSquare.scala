fa9d6ad8ac99cc7da33abcfde29289fbdb21d3a4
Andrea Rosà <andrea.rosa@usi.ch>
2019-04-15 19:11:34 +0200

    Format fixed

--- benchmarks/apache-spark/src/main/scala/org/renaissance/apache/spark/ChiSquare.scala
+++ benchmarks/apache-spark/src/main/scala/org/renaissance/apache/spark/ChiSquare.scala
@@ -69,7 +69,7 @@ class ChiSquare extends RenaissanceBenchmark {
         val raw = line.split(" ").map(_.toDouble)
         new LabeledPoint(raw.head, Vectors.dense(raw.tail))
       }
-        .cache()
+      .cache()
   }
 
   def setUpSpark() = {
