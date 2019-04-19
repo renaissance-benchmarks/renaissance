ebdd462002b6fea0e749a7d03b4eec020e0e3d8e
Lubomír Bulej <bulej@d3s.mff.cuni.cz>
2019-04-20 00:53:54 +0200

    Get rid of extra whitespace

--- benchmarks/jdk-concurrent/src/main/java/org/renaissance/jdk/concurrent/JavaKMeans.java
+++ benchmarks/jdk-concurrent/src/main/java/org/renaissance/jdk/concurrent/JavaKMeans.java
@@ -23,16 +23,16 @@ public final class JavaKMeans {
   private final int clusterCount;
 
   private final int iterationCount;
-  
+
   private final int vectorLength;
 
   private final int forkThreshold;
 
   //
-  
+
   private final ForkJoinPool forkJoin = new ForkJoinPool();
 
-  
+
   public JavaKMeans(
     int dimension, int vectorLength, int clusterCount,
     int iterationCount, int threadCount
@@ -44,7 +44,7 @@ public final class JavaKMeans {
     this.forkThreshold = vectorLength / (4 * threadCount) + 1;
   }
 
- 
+
   public Vector<Double[]> run() throws InterruptedException, ExecutionException {
     final Vector<Double[]> data = generateData(vectorLength);
 
@@ -55,11 +55,11 @@ public final class JavaKMeans {
       final AssignmentTask assignmentTask = new AssignmentTask(data, centroids);
       final UpdateTask updateTask = new UpdateTask(forkJoin.invoke(assignmentTask));
       final Map<Double[], Vector<Double[]>> clusters = forkJoin.invoke(updateTask);
-      
+
       centroids.clear();
       centroids.addAll(clusters.keySet());
     }
-    
+
     forkJoin.awaitQuiescence(1, TimeUnit.SECONDS);
     return centroids;
   }
@@ -87,7 +87,7 @@ public final class JavaKMeans {
         base, base + 1, base * 4, base * 2, base * 3,
     };
   }
-  
+
 
   public void tearDown() {
     try {
@@ -100,9 +100,9 @@ public final class JavaKMeans {
   }
 
   //
-  
+
   final class AssignmentTask extends RecursiveTask<Map<Double[], Vector<Double[]>>> {
-    
+
     private final Vector <Double []> data;
 
     private final int fromInclusive;
@@ -110,19 +110,19 @@ public final class JavaKMeans {
     private final int toExclusive;
 
     private final int elementCount;
-    
+
     private final Vector <Double []> centroids;
-    
+
 
     public AssignmentTask(
       final Vector <Double []> data, final Vector <Double []> centroids
     ) {
       this (data, centroids, 0, data.size());
     }
-    
-    
+
+
     public AssignmentTask(
-      final Vector <Double []> data, final Vector <Double []> centroids, 
+      final Vector <Double []> data, final Vector <Double []> centroids,
       final int fromInclusive, final int toExclusive
     ) {
       this.data = data;
@@ -132,36 +132,36 @@ public final class JavaKMeans {
       this.elementCount = toExclusive - fromInclusive;
     }
 
-    
+
     @Override
     protected Map<Double[], Vector<Double[]>> compute() {
       if (elementCount < forkThreshold) {
         return assignToClusters();
-        
+
       } else {
         final int middle = fromInclusive + elementCount / 2;
-        final ForkJoinTask<Map<Double[], Vector<Double[]>>> leftTask = 
+        final ForkJoinTask<Map<Double[], Vector<Double[]>>> leftTask =
           new AssignmentTask(data, centroids, fromInclusive, middle).fork();
         final ForkJoinTask<Map<Double[], Vector<Double[]>>> rightTask =
           new AssignmentTask(data, centroids, middle, toExclusive).fork();
-        
+
         return merge(leftTask.join(), rightTask.join());
       }
     }
 
-    
+
     private Map<Double[], Vector<Double[]>> assignToClusters() {
       final int[] nearestCentroidIndex = findNearestCentroid();
       return collectClusters(nearestCentroidIndex);
     }
 
-   
+
     private int[] findNearestCentroid() {
       final int[] result = new int[elementCount];
 
       for (int dataIndex = fromInclusive; dataIndex < toExclusive; dataIndex++) {
         final Double[] element = data.elementAt(dataIndex);
-        
+
         double min = Double.MAX_VALUE;
         for (int centroidIndex = 0; centroidIndex < centroids.size(); centroidIndex++) {
           final double distance = distance(element, centroids.elementAt(centroidIndex));
@@ -175,21 +175,21 @@ public final class JavaKMeans {
       return result;
     }
 
-    
+
     private Map<Double[], Vector<Double[]>> collectClusters(final int[] centroidIndices) {
       final Map<Double[], Vector<Double[]>> result = new HashMap<>();
-      
+
       for (int dataIndex = fromInclusive; dataIndex < toExclusive; dataIndex++) {
         final int centroidIndex = centroidIndices[dataIndex - fromInclusive];
         final Double[] centroid = centroids.elementAt(centroidIndex);
         final Double[] element = data.elementAt(dataIndex);
         result.computeIfAbsent(centroid, k -> new Vector<>()).add(element);
       }
-      
+
       return result;
     }
 
-    
+
     private Double distance(final Double[] x, final Double[] y) {
       //
       // Calculates Euclidean distance between the two points. Note that we
@@ -202,28 +202,28 @@ public final class JavaKMeans {
 
       return result;
     }
-    
-    
+
+
     private <T> Map<T, Vector<T>> merge(
       final Map<T, Vector<T>> left, final Map<T, Vector<T>> right
     ) {
       final Map<T, Vector<T>> result = new HashMap<>(left);
-      
+
       right.forEach((key, val) -> result.merge(
         key, val, (l, r) -> { l.addAll (r); return l; }
       ));
 
       return result;
     }
-    
+
   }
-  
+
   //
 
   final class UpdateTask extends RecursiveTask<Map <Double[], Vector<Double[]>>> {
-    
+
     private final Map <Double [], Vector<Double[]>> clusters;
-    
+
     public UpdateTask(Map <Double [], Vector<Double[]>> clusters) {
       this.clusters = clusters;
     }
@@ -236,19 +236,19 @@ public final class JavaKMeans {
       ));
     }
 
-    
+
     private Double[] average(final Vector<Double[]> elements) {
       return Arrays.stream(unboxedAverage(elements)).boxed().toArray(Double[]::new);
     }
 
-    
+
     private double[] unboxedAverage(final Vector<Double[]> elements) {
       final VectorSumTask sumTask = new VectorSumTask(elements);
       final double[] vectorSums = getPool().invoke(sumTask);
       return div(vectorSums, elements.size());
     }
 
-    
+
     private double[] div(double[] values, int divisor) {
       final double[] result = new double[values.length];
       for (int i = 0; i < values.length; i++) {
@@ -257,11 +257,11 @@ public final class JavaKMeans {
 
       return result;
     }
-    
+
   }
 
   //
-  
+
   final class VectorSumTask extends RecursiveTask<double[]> {
 
     private final Vector<Double[]> data;
@@ -277,7 +277,7 @@ public final class JavaKMeans {
       this(data, 0, data.size());
     }
 
-    
+
     public VectorSumTask(
       final Vector<Double[]> data,
       final int fromInclusive, final int toExclusive
@@ -288,7 +288,7 @@ public final class JavaKMeans {
       this.elementCount = toExclusive - fromInclusive;
     }
 
-    
+
     @Override
     protected double[] compute() {
       if (elementCount < forkThreshold) {
@@ -296,9 +296,9 @@ public final class JavaKMeans {
 
       } else {
         final int middle = fromInclusive + elementCount / 2;
-        final ForkJoinTask<double[]> leftTask = 
+        final ForkJoinTask<double[]> leftTask =
           new VectorSumTask(data, fromInclusive, middle).fork();
-        final ForkJoinTask<double[]> rightTask = 
+        final ForkJoinTask<double[]> rightTask =
           new VectorSumTask(data, middle, toExclusive).fork();
         return add(leftTask.join(), rightTask.join());
       }
@@ -311,14 +311,14 @@ public final class JavaKMeans {
       for (int i = 0; i < dimension; i++) {
         result[i] = x[i] + y[i];
       }
-      
+
       return result;
     }
-    
+
 
     private double[] vectorSum() {
       final double[] result = new double[dimension];
-      
+
       for (int i = fromInclusive; i < toExclusive; i++) {
         accumulate(data.elementAt(i), result);
       }
@@ -332,7 +332,7 @@ public final class JavaKMeans {
         acc[i] += val[i];
       }
     }
-    
+
   }
 
 }
