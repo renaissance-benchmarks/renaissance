f1f5bf42001b0b347ae0dc331756fc3da3f382f5
Alex Villazon <alex.villazon@gmail.com>
2019-04-28 17:32:25 -0400

    Fixes issues with winutils.exe on Windows + indent

--- benchmarks/apache-spark/src/main/scala/org/renaissance/apache/spark/HadoopUtil.scala
+++ benchmarks/apache-spark/src/main/scala/org/renaissance/apache/spark/HadoopUtil.scala
@@ -6,12 +6,13 @@ import java.io.FileOutputStream
 
 object HadoopUtil {
 
-  // For Spark version on renaissance (2.0.0) the Hadoop version is 2.2.0
-  // For Windows, the binary zip is not included in the dependencies
-  // We include in the resource winutils.exe from
-  // https://github.com/srccodes/hadoop-common-2.2.0-bin
-  // If Spark version is updated in future releases of renaissance,
-  // the file must be upgraded to the corresponding Hadoop version
+  /* For Spark version on renaissance (2.0.0) the Hadoop version is 2.2.0
+     For Windows, the binary zip is not included in the dependencies
+     We include in the resource winutils.exe from
+     https://github.com/srccodes/hadoop-common-2.2.0-bin
+     If Spark version is updated in future releases of renaissance,
+     the file must be upgraded to the corresponding Hadoop version
+   */
   val inputFile = "/winutils.exe"
 
   def setUpHadoop(tempDirPath: Path) = {
