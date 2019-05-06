f3fa9f64bd3da69df126bbb4433385033756a3d7
Francois Farquet <francois.farquet@oracle.com>
2019-05-06 06:52:53 +0200

    Made smaller version of fj-kmeans

--- benchmarks/jdk-concurrent/src/main/scala/org/renaissance/jdk/concurrent/FjKmeans.scala
+++ benchmarks/jdk-concurrent/src/main/scala/org/renaissance/jdk/concurrent/FjKmeans.scala
@@ -11,25 +11,34 @@ class FjKmeans extends RenaissanceBenchmark {
 
   def licenses = License.create(License.APACHE2)
 
-  private val DIMENSION = 5
-
-  private val VECTOR_LENGTH = 500000
-
-  private val CLUSTER_COUNT = 5
-
-  private val ITERATION_COUNT = 50
-
-  private val LOOP_COUNT = 4
-
+  // TODO: Consolidate benchmark parameters across the suite.
+  //  See: https://github.com/D-iii-S/renaissance-benchmarks/issues/27
   private val THREAD_COUNT = Runtime.getRuntime.availableProcessors
+
+  private var DIMENSION = 5
+
+  private var VECTOR_LENGTH = 500000
+
+  private var CLUSTER_COUNT = 5
+
+  private var ITERATION_COUNT = 50
+
+  private var LOOP_COUNT = 4
 
   private var benchmark: JavaKMeans = null
 
   private var data: java.util.List[Array[java.lang.Double]] = null
 
   override def setUpBeforeAll(c: Config): Unit = {
+    if (c.functionalTest) {
+      DIMENSION = 5
+      VECTOR_LENGTH = 500
+      CLUSTER_COUNT = 5
+      ITERATION_COUNT = 50
+      LOOP_COUNT = 4
+    }
     benchmark = new JavaKMeans(DIMENSION, THREAD_COUNT)
-    data = JavaKMeans.generateData(VECTOR_LENGTH, DIMENSION, CLUSTER_COUNT);
+    data = JavaKMeans.generateData(VECTOR_LENGTH, DIMENSION, CLUSTER_COUNT)
   }
 
   override def runIteration(c: Config): Unit = {
