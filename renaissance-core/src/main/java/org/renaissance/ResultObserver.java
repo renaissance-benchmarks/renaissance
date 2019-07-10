7103d767cc9d561c007bfdf173dadeda73399bc3
Lubomír Bulej <bulej@d3s.mff.cuni.cz>
2019-07-10 17:54:07 +0200

    Update documentation on ResultObserver

--- renaissance-core/src/main/java/org/renaissance/ResultObserver.java
+++ renaissance-core/src/main/java/org/renaissance/ResultObserver.java
@@ -1,15 +1,18 @@
 package org.renaissance;
 
 public interface ResultObserver {
-  /** Callback when new result is obtained.
-   *
-   * Will be called after iteration is completed, i.e. never inside measured
-   * loop.
+
+  /**
+   * Called when a new result is obtained. Will be called after benchmark repetition is
+   * completed, i.e. never inside the measured code.
    *
    * @param benchmark Name of the benchmark.
    * @param metric Result name (e.g. branch-misses).
    * @param value Actual value.
    */
-  public void onNewResult(String benchmark, String metric, long value);
-  public void onExit();
+  void onNewResult(String benchmark, String metric, long value);
+
+  // TODO Plugins should register for benchmark life-cycle events separately
+  void onExit();
+
 }
