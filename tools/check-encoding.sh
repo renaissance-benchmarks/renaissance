renaissance-benchmarks/renaissance

commit 1cccc888335af169252373087934dd62189b62a8

Travis: check *.java are in pure ASCII (fixes #43)

.travis.yml
@@ -46,6 +46,11 @@ jobs:
         - ./tools/sbt/bin/sbt assembly
         - java -jar ./target/renaissance-gpl-$RENAISSANCE_VERSION.jar --readme && git diff --exit-code -- README.md CONTRIBUTION.md
 
+    - stage: "Basic checks"
+      name: "Content encoding of Java sources"
+      script:
+        - "! grep -P -nH -r '--include=*.java' -e '[^\x00-\x7F]' ."
+
     - <<: *bundle
       os: osx
     - <<: *bundle

commit c19e0dd528fec510083de7e3c914a8fc9e0ab0b4

Fix Travis (#180)
Broken since 1cccc888335af16925237.

.travis.yml
@@ -49,7 +49,7 @@ jobs:
     - stage: "Basic checks"
       name: "Content encoding of Java sources"
       script:
-        - "! grep -P -nH -r '--include=*.java' -e '[^\x00-\x7F]' ."
+        - ./tools/check-encoding.sh
 
     - <<: *bundle
       os: osx

tools/check-encoding.sh
@@ -1,0 +1,6 @@
+#!/bin/sh
+
+if grep -P -nH -r '--include=*.java' -e '[^\x00-\x7F]' .; then
+    exit 1;
+fi
+