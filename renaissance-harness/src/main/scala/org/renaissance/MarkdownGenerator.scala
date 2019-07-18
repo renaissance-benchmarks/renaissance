11de73c56e73e5062ca3a3e30d2e455a5480247b
Lubomír Bulej <bulej@d3s.mff.cuni.cz>
2019-07-18 12:07:19 +0200

    Split off generation of markdown docs into MarkdownGenerator
    The generator should be only run at build time, there is no need
    to handle this in the suite. For now, the generator still relies
    on bits of the RenaissanceSuite, e.g., the help text from the
    config parser.

---
+++ renaissance-harness/src/main/scala/org/renaissance/MarkdownGenerator.scala
@@ -0,0 +1,459 @@
+package org.renaissance
+
+import java.io.File
+
+import org.apache.commons.io.FileUtils
+import org.renaissance.util.BenchmarkInfo
+import org.renaissance.util.BenchmarkRegistry
+import org.renaissance.util.ModuleLoader
+
+import scala.collection.JavaConverters._
+
+/**
+ * Generates README.md and CONTRIBUTION.md files for the suite.
+ * This is currently part of the harness because is relies on
+ * Scala string interpolation and collects information from
+ * several (harness-internal) places.
+ */
+object MarkdownGenerator {
+
+  private val benchmarks = BenchmarkRegistry.createUsingProperties(
+    getClass.getResourceAsStream("/benchmark-details.properties")
+  )
+
+  def main(args: Array[String]): Unit = {
+    FileUtils.write(
+      new File("README.md"),
+      readme,
+      java.nio.charset.StandardCharsets.UTF_8,
+      false
+    )
+
+    FileUtils.write(
+      new File("CONTRIBUTION.md"),
+      contribution,
+      java.nio.charset.StandardCharsets.UTF_8,
+      false
+    )
+
+    println("README.md and CONTRIBUTION.md updated.")
+  }
+
+  private def formatBenchmarkListMarkdown = {
+    def formatItem(b: BenchmarkInfo) = {
+      s"- `${b.name}` - ${b.summary} (default repetitions: ${b.repetitions})"
+    }
+
+    val result = new StringBuffer
+    for ((group, benches) <- benchmarks.byGroup().asScala) {
+      result.append(s"#### ${group}").append("\n\n")
+
+      val sortedBenches = benches.asScala.toSeq.sortBy(_.name)
+      result.append(sortedBenches.map(b => formatItem(b)).mkString("\n\n")).append("\n\n")
+    }
+
+    result.toString
+  }
+
+  def formatBenchmarkTableMarkdown = {
+    def formatRow(b: BenchmarkInfo) = {
+      s"| ${b.name} | ${b.printableLicenses} | ${b.distro} |"
+    }
+
+    benchmarks.byName().asScala.values.map(b => formatRow(b)).mkString("\n")
+  }
+
+  val logoUrl = "https://github.com/renaissance-benchmarks/renaissance/" +
+    "raw/master/website/resources/images/mona-lisa-round.png"
+
+  lazy val readme = s"""
+
+## Renaissance Benchmark Suite
+
+<p align="center">
+  <img height="180px" src="${logoUrl}"/>
+</p>
+
+
+The Renaissance Benchmark Suite aggregates common modern JVM workloads,
+including, but not limited to, Big Data, machine-learning, and functional programming.
+The suite is intended to be used to optimize just-in-time compilers, interpreters, GCs,
+and for tools such as profilers, debuggers, or static analyzers, and even different hardware.
+It is intended to be an open-source, collaborative project,
+in which the community can propose and improve benchmark workloads.
+
+
+### Building the suite
+
+To build the suite and create the so-called fat JAR (or super JAR), you only
+need to run `sbt` built tool:
+
+```
+$$ tools/sbt/bin/sbt assembly
+```
+
+This will retrieve all the dependencies, compile all the benchmark projects and the harness,
+bundle the JARs and create the final JAR under `target` directory.
+
+
+### Running the benchmarks
+
+To run a Renaissance benchmark, you need to have a JRE installed.
+This allows you to execute the following `java` command:
+
+```
+$$ java -jar '<renaissance-home>/target/renaissance-gpl-${RenaissanceSuite.renaissanceVersion}.jar' <benchmarks>
+```
+
+Above, the `<renaissance-home>` is the path to the root directory of the Renaissance distribution,
+and `<benchmarks>` is the list of benchmarks that you wish to run.
+For example, you can specify `scala-kmeans` as the benchmark.
+
+
+#### Complete list of command-line options
+
+The following is a complete list of command-line options.
+
+```
+${RenaissanceSuite.parser.usage}
+```
+
+
+### List of benchmarks
+
+The following is the complete list of benchmarks, separated into groups.
+
+${formatBenchmarkListMarkdown}
+
+
+### Run policies
+
+The suite is designed to support multiple ways of executing a benchmark --
+for example, a fixed number of iterations, or a fixed amount of time.
+This logic is encapsulated in run policies. Current policies include:
+
+${Policy.descriptions.asScala.map { case (k, v) => s"- `$k` -- $v\n" }.mkString("\n")}
+
+
+### Plugins and interfacing with external tools
+
+If you are using an external tool to inspect a benchmark, such as an instrumentation agent,
+or a profiler, then you will need to make this tool aware of when a benchmark iteration
+is starting and when it is ending.
+To allow this, the suite allows specifying custom plugins, which are notified when necessary.
+Here is an example of how to implement a plugin:
+
+```
+class MyPlugin extends ${classOf[Plugin].getSimpleName} {
+  def onCreation() = {
+    // Initialize the plugin after it has been created.
+  }
+  def beforeIteration(policy: ${classOf[Policy].getSimpleName}) = {
+    // Notify the tool that a benchmark iteration is about to start.
+  }
+  def afterIteration(policy: ${classOf[Policy].getSimpleName}) = {
+    // Notify the tool that the benchmark iteration has ended.
+  }
+}
+```
+
+Here, the ${classOf[Policy].getSimpleName} argument describes
+the current state of the benchmark.
+
+
+### JMH support
+
+You can also build and run Renaissance with JMH. To build a JMH-enabled JAR, run:
+
+```
+$$ tools/sbt/bin/sbt renaissanceJmh/jmh:assembly
+```
+
+To run the benchmarks using JMH, you can execute the following `java` command:
+
+```
+$$ java -jar '${RenaissanceSuite.jmhTargetPath}/${RenaissanceSuite.jmhJarPrefix}-${RenaissanceSuite.renaissanceVersion}.jar'
+```
+
+
+### Contributing
+
+Please see the [CONTRIBUTION](CONTRIBUTION.md) page for a description of the contributing process.
+
+### Licensing
+
+The Renaissance Suite comes in two distributions,
+and is available under both the MIT license and the GPL3 license.
+The GPL distribution with all the benchmarks is licensed under the GPL3 license,
+while the MIT distribution includes only those benchmarks that themselves
+have less restrictive licenses.
+
+Depending on your needs, you can use either of the two distributions.
+The following table contains the licensing information of all the benchmarks:
+
+| Benchmark     | Licenses      | Renaissance Distro |
+| ------------- | ------------- |:------------------:|
+${formatBenchmarkTableMarkdown}
+
+
+### Design overview
+
+The Renaissance benchmark suite is organized into several `sbt` projects:
+
+- the `renaissance-core` folder that contains a set of *core* classes
+  (common interfaces and a harness launcher)
+- the `renaissance-harness` folder that contains the actual harness
+- the `benchmarks` folder contains a set of *subprojects*, each containing a set of benchmarks
+  for a specific domain (and having a separate set of dependencies)
+
+The *core* project is written in pure Java, and it contains the basic benchmark API.
+Its most important class is `${classOf[RenaissanceBenchmark].getSimpleName}`,
+which must be extended by a concrete benchmark implementation, and the
+annotations in the `${classOf[Benchmark].getSimpleName}` class, which are
+used to set static information about a benchmark, such as a summary or
+detailed description.
+Consequently, each *subproject* depends on the *core* project.
+
+Interfaces of *core* are loaded (when Renaissance is started) by the default
+classloader. Every other class (including harness and individual benchmarks)
+is loaded by a separate classloader. This separation was done so that there
+are never clashes between the different dependencies of the different projects.
+Because each benchmark may depend on different versions of external libraries.
+
+The *harness* project implements the functionality that is necessary
+to parse the input arguments, to run the benchmarks, to generate documentation,
+and so on. The *harness* is written in Scala and is loaded by the *core*
+in a separate classloader to ensure clean environment for running the
+benchmarks.
+
+The JARs of the subprojects (benchmarks and harness) are copied as generated
+*resources* and embedded into the resulting JAR artifact.
+
+```
+renaissance-core
+  ^
+  | (classpath dependencies)
+  |
+  |-- renaissance harness
+  |
+  |-- benchmark one
+  | `-- dependencies for benchmark one
+  |
+  |-- ...
+  |
+  `-- benchmark n
+```
+
+When the harness is started, it uses the input arguments to select the benchmark,
+and then unpacks the JARs of the corresponding benchmark group into a scratch folder.
+The harness then creates a classloader with the unpacked JARs and loads the benchmark group.
+The class loader is created directly below the default class loader. Because
+the default class loader contains only base JRE classes and common interfaces
+of *core*, it ensures that dependencies of a benchmark are never mixed with any
+dependencies of any other benchmark or the harness.
+
+```
+        boot class loader (JDK)
+                   ^
+                   |
+          system class loader
+                (core)
+         ^                   ^
+         |                   |
+  URL class loader    URL class loader
+     (harness)          (benchmark)
+```
+
+We need to do this to, e.g., avoid accidentally resolving the wrong class
+by going through the system class loader (this can easily happen with,
+e.g. Apache Spark and Scala, due to the way that Spark internally resolves some classes).
+
+You can see the further details of the build system in the top-level `build.sbt` file,
+in the `renaissance-suite.scala` file and in `${classOf[ModuleLoader].getSimpleName}`.
+
+
+"""
+
+  lazy val contribution = s"""
+
+## Contribution Guide
+
+### Code organization and internals
+
+The code is organized into three main parts:
+
+- `renaissance-core`: these are the core APIs that a benchmark needs to work with,
+  such as the runtime configuration, the benchmark base class or the policy.
+- `renaissance-harness`: this is the overall suite project, which is responsible for
+  parsing the arguments, loading the classes, and running the benchmark.
+- a set of projects in the `benchmarks` directory: these are the individual groups of benchmarks
+  that share a common set of dependencies. A group is typically thematic, relating to
+  a particular framework or a JVM language, and benchmarks in different groups depend
+  on a different set of libraries, or even the same libraries with different versions.
+  The bundling mechanism of the suite takes care that the dependencies of the different groups
+  never clash with each other.
+
+
+### Adding a new benchmark
+
+To add a new benchmark to an existing group, identify the respective project
+in the `benchmarks` directory, and add a new top-level Scala class
+that extends the `${classOf[RenaissanceBenchmark].getSimpleName}` interface.
+
+Here is an example:
+
+```
+import org.renaissance._
+import org.renaissance.Benchmark._
+
+@Summary("Runs some performance-critical Java code.")
+final class MyJavaBenchmark extends ${classOf[RenaissanceBenchmark].getSimpleName} {
+  override protected def runIteration(config: ${classOf[Config].getSimpleName}): BenchmarkResult = {
+    // This is the benchmark body, which in this case calls some Java code.
+    JavaCode.runSomeJavaCode()
+    // Return object for later validation of the iteration.
+    return new MyJavaBenchmarkResult()
+  }
+}
+```
+
+Above, the name of the benchmark will be automatically generated from the class name.
+In this case, the name will be `${RenaissanceBenchmark.kebabCase("MyJavaBenchmark")}`.
+
+To create a new group of benchmarks (for example, benchmarks that depend on a new framework),
+create an additional `sbt` project in the `benchmarks` directory,
+using an existing project, such as `scala-stdlib`, as an example.
+The project will be automatically picked up by the build system
+and included into the Renaissance distribution.
+
+Once the benchmark has been added, one needs to make sure to be compliant with the code formatting of the project
+(rules defined in `.scalafmt.conf`).
+A convenient sbt task can do that check:
+```
+$$ tools/sbt/bin/sbt renaissanceFormatCheck
+```
+
+Another one can directly update the source files to match the desired format:
+```
+$$ tools/sbt/bin/sbt renaissanceFormat
+```
+
+Moreover, the content of the README and CONTRIBUTION files are automatically generated from the codebase.
+Updating those files can be done with the `--readme` command-line flag. Using sbt, one would do:
+```
+$$ tools/sbt/bin/sbt runMain ${classOf[Launcher].getName} --readme
+```
+
+### IDE development
+
+#### IntelliJ
+
+In order to work on the project with IntelliJ, one needs to install the following plugins :
+  - `Scala` : part of the codebase uses Scala and Renaissance is organized in sbt projects.
+  - `scalafmt` (optional) : adds an action that can be triggered with `Code -> Reformat with scalafmt`
+  which will reformat the code according to the formatting rule defined in `.scalafmt.conf`
+  (same as the `renaissanceFormat` sbt task).
+
+Then, the root of this repository can be opened as an IntelliJ project.
+
+### Benchmark evaluation and release process
+
+In the open-source spirit, anyone can propose an additional benchmark by opening a pull request.
+The code is then inspected by the community -- typically, the suite maintainers are involved
+in the review, but anybody else is free join the discussion.
+During the discussion, the reviewers suggest the ways in which
+the benchmark could be improved.
+
+Before submitting a pull request, it is recommendable to open an issue first,
+and discuss the benchmark proposal with the maintainers.
+
+
+#### Benchmark criteria
+
+Here is some of the quality criteria that a new benchmark should satisfy:
+
+- *Stylistically conformant*: the benchmark code must conform to existing formatting
+  and stylistic standards in the suite.
+- *Meaningful*: it must represent a meaningful workload that is either frequently executed,
+  or it consists of code patterns and coding styles that are desired or arguably preferable,
+  or it encodes some important algorithm, a data structure or a framework,
+  or is significant in some other important way.
+  If the benchmark is automatically generated by some tool,
+  then it must be accompanied with a detailed explanation of why the workload is useful.
+- *Observable*: it must constitute a run that is observable and measurable. For example,
+  the run should last sufficiently long so that it is possible to accurately measure
+  its running time, gather profiling information or observe a performance effect.
+  Typically, this means that the benchmark duration should be between 0.5 and 10 seconds.
+- *Deterministic*: the performance effects of the benchmark should be reproducible.
+  Even if the benchmark consists of, for example, concurrent code that is inherently
+  non-deterministic, the benchmark should behave relatively deterministically in terms
+  of the number of threads that it creates, the objects it allocates, and the total amount
+  of computation that it needs to do. For example, each of the benchmark repetitions should have
+  a relatively stable running time on major JVMs.
+- *New*: it must contain some new functionality that is not yet reflected in any of the existing
+  benchmarks. If there is significant overlap with an existing benchmark, then it should be
+  explained why the new benchmark is necessary.
+- *Self-contained*: it must be runnable within a single JVM instance, without any additional
+  software installation, operating system functionality, operating system services,
+  other running processes, online services, networked deployments or similar.
+  The JVM installation should be sufficient to run all the code of the benchmark.
+  For example, if the benchmark is exercising Apache Spark, then the workers should run
+  in the local mode, and if the benchmark targets a database, then it should be embedded.
+  The benefit is that the performance effects of the benchmark are easy to measure,
+  and the code is reproducible everywhere.
+- *Open-source*: the benchmark must consist of open-source code, with well-defined licenses.
+
+
+#### Code of Conduct
+
+We would also like to point you to the
+[Renaissance Code of Conduct](https://github.com/renaissance-benchmarks/renaissance/blob/master/CODE-OF-CONDUCT.md). As a member
+of the Renaissance community, make sure that you follow it to guarantee an enjoyable experience for every member of
+the community.
+
+#### Release process
+
+While the open-source process is designed to accept contributions on an ongoing basis,
+we expect that this benchmark suite will grow considerably over the course of time.
+We will therefore regularly release snapshots of this suite, which will be readily available.
+These will be known as *minor releases*.
+
+Although we will strive to have high-quality, meaningful benchmarks, it will be necessary
+to proliferate the most important ones, and publish them as *major releases*.
+This way, researchers and developers will be able to test their software
+against those benchmarks that were deemed most relevant.
+A major release will still include all the benchmarks in the suite, but the list of highlighted
+benchmarks might evolve between major releases.
+
+Once a year, a committee convenes and discusses which important benchmarks were contributed
+since the last release, and should become highlighted in the next major release.
+The committee consists of members from various universities and companies,
+who are involved in research and development in virtual machine, compiler, memory management,
+static and dynamic analysis, and performance engineering.
+
+The committee goes over the benchmark list, and inspect the new ones since the last release.
+The members can recommend specific benchmarks for inclusion in the highlighted list,
+and can present their arguments about why those benchmarks should be included.
+In addition, the members can recommend the exclusion of some benchmarks.
+The committee members then vote, and the majority is the basis for a decision.
+
+The new major release is then bundled and the binaries are made available publicly.
+
+The current members of the committee are:
+
+- Walter Binder, Universita della Svizzera italiana
+- Steve Blackburn, Australian National University
+- Lubomir Bulej, Charles University
+- Gilles Duboscq, Oracle Labs
+- Francois Farquet, Oracle Labs
+- Vojtech Horky, Charles University
+- David Leopoldseder, Johannes Kepler University Linz
+- Guillaume Martres, Ecole Polytechnique Federale de Lausanne
+- Aleksandar Prokopec, Oracle Labs
+- Andrea Rosa, Universita della Svizzera italiana
+- Denys Shabalin, Ecole Polytechnique Federale de Lausanne
+- Petr Tuma, Charles University
+- Alex Villazon, Universidad Privada Boliviana
+"""
+
+}
