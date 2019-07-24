624974ca4608c6f089bb22ebb27c162e7cec9eea
Lubomír Bulej <bulej@d3s.mff.cuni.cz>
2019-07-25 01:45:56 +0200

    Move Pair into util package and make it package private
    It is only used by ModuleLoader now

--- renaissance-core/src/main/java/org/renaissance/util/ModuleLoader.java
+++ renaissance-core/src/main/java/org/renaissance/util/ModuleLoader.java
@@ -18,7 +18,6 @@ import java.util.Scanner;
 import java.util.logging.Logger;
 
 import org.renaissance.Launcher;
-import org.renaissance.Pair;
 
 public final class ModuleLoader {
   private static final URL[] URL_ARRAY_TYPE = new URL[0];
