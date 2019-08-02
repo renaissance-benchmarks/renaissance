e4ffe6d31425cc01d82ed205098b7caa9c2dd0ea
Lubomír Bulej <bulej@d3s.mff.cuni.cz>
2019-08-02 16:37:31 +0200

    Update Benchmark documentation comments

--- renaissance-core/src/main/java/org/renaissance/Benchmark.java
+++ renaissance-core/src/main/java/org/renaissance/Benchmark.java
@@ -2,6 +2,24 @@ package org.renaissance;
 
 import java.lang.annotation.*;
 
+/**
+ * Represents a Renaissance benchmark. Defines the methods that need to be
+ * implemented by each benchmark and serves as a name space for benchmark
+ * meta-data annotations.
+ * <p>
+ * Each benchmark must provide at least the {@link #runIteration(BenchmarkContext)} method,
+ * which implements the measured part of the benchmark workload. Other methods are
+ * optional and provide a benchmark with more fine-grained control over its
+ * life-cycle.
+ * <p>
+ * The {@link #runIteration(BenchmarkContext)} method must return an instance
+ * of {@link BenchmarkResult}, which is used by the harness to validate the
+ * result of the measured operation.
+ * <p>All {@link Benchmark} methods receive an instance of {@link BenchmarkContext}
+ * as an argument, which allows them to query values of configurable
+ * parameters, retrieve distribution JAR resources, or ask for harness-managed
+ * temporary directories.
+ */
 public interface Benchmark {
 
   /**
@@ -13,9 +31,7 @@ public interface Benchmark {
   @Target(ElementType.TYPE)
   @Retention(RetentionPolicy.RUNTIME)
   @interface Name {
-
     String value();
-
   }
 
 
@@ -29,9 +45,7 @@ public interface Benchmark {
   @Target(ElementType.TYPE)
   @Retention(RetentionPolicy.RUNTIME)
   @interface Group {
-
     String value();
-
   }
 
 
@@ -42,9 +56,7 @@ public interface Benchmark {
   @Target(ElementType.TYPE)
   @Retention(RetentionPolicy.RUNTIME)
   @interface Summary {
-
     String value();
-
   }
 
 
@@ -55,9 +67,7 @@ public interface Benchmark {
   @Target(ElementType.TYPE)
   @Retention(RetentionPolicy.RUNTIME)
   @interface Description {
-
     String value();
-
   }
 
 
@@ -69,10 +79,8 @@ public interface Benchmark {
   @Documented
   @Target(ElementType.TYPE)
   @Retention(RetentionPolicy.RUNTIME)
-  public @interface Licenses {
-
+  @interface Licenses {
     License[] value();
-
   }
 
 
@@ -86,52 +94,87 @@ public interface Benchmark {
   @Documented
   @Target(ElementType.TYPE)
   @Retention(RetentionPolicy.RUNTIME)
-  public @interface Repetitions {
-
+  @interface Repetitions {
     int value();
-
   }
 
 
+  /**
+   * Container for {@link Configuration} annotations. Does not have to be
+   * used explicitly since Java 8 which introduced the {@link Repeatable}
+   * annotation. However, older Scala versions do not support annotations
+   * types tagged as {@link Repeatable}, therefore the container type has
+   * to be used explicitly.
+   */
   @Documented
   @Target(ElementType.TYPE)
   @Retention(RetentionPolicy.RUNTIME)
-  public @interface Configurations {
-
+  @interface Configurations {
     Configuration[] value() default {};
-
   }
 
 
+  /**
+   * Defines a named benchmark configuration that a benchmark can be run with.
+   * <p>
+   * Each configuration must have a {@link #name() name} and consists of the
+   * parameters declared using the {@link Parameter} annotation, initially set
+   * to their default values.
+   * <p>
+   * The {@link #settings() settings} attribute is optional and allows overriding
+   * the values of declared configurable parameters (if not set, the parameters
+   * are left at their default values). The attribute is an array of strings,
+   * each containing a single key-value pair assignment in the form
+   * {@code "<parameter_name> = <parameter_value>"}, which overrides the value
+   * of the given parameter. Only parameters that have been previously
+   * declared using the {@link Parameter} annotation can be overriden.
+   * <p>
+   * The {@link #summary() summary} description is optional but recommended.
+   */
   @Documented
   @Target(ElementType.TYPE)
   @Repeatable(Configurations.class)
   @Retention(RetentionPolicy.RUNTIME)
-  public @interface Configuration {
+  @interface Configuration {
     String name();
     String[] settings() default {};
+    String summary() default "";
   }
 
 
+  /**
+   * Container for {@link Parameter} annotations. Does not have to be
+   * used explicitly since Java 8 which introduced the {@link Repeatable}
+   * annotation. However, older Scala versions do not support annotations
+   * types tagged as {@link Repeatable}, therefore the container type has
+   * to be used explicitly.
+   */
   @Documented
   @Target(ElementType.TYPE)
   @Retention(RetentionPolicy.RUNTIME)
-  public @interface Parameters {
-
+  @interface Parameters {
     Parameter[] value() default {};
-
   }
 
+
+  /**
+   * Defines a configurable benchmark parameter that a benchmark can query.
+   * <p>
+   * Each parameter must have a name and a default value represented as
+   * {@link String}. A summary description of the parameter is optional but
+   * recommended. The default values of configurable parameters associated
+   * with a benchmark class make up a default configuration (the configuration
+   * is named {@code default} and does not need to be declared explicitly).
+   */
   @Documented
   @Target(ElementType.TYPE)
   @Repeatable(Parameters.class)
   @Retention(RetentionPolicy.RUNTIME)
-  public @interface Parameter {
+  @interface Parameter {
     String name();
     String defaultValue() default "";
     String summary() default "";
   }
-
 
   //
 
