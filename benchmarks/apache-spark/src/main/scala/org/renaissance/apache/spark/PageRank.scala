dd75ceb80128817181d2ea6a53629b97cb44a995
Lubomír Bulej <bulej@d3s.mff.cuni.cz>
2019-08-25 17:35:54 +0200

    Avoid unnecessary disk IO and add hash-based validation
    Instead of unpacking the input file to disk letting Spark read it into
    an RDD, we directly read the contents of the zipped file into a
    collection of lines and build an RDD using the SparkContext's
    parallelize() method.
    
    We also avoid writing the ranks from the last executed operation
    to disk.
    
    In addition, add a hash-based validation of the results. The computed
    ranks are rounded to 8 decimal places and ordered by the web site id
    so as to avoid numerical instability.

--- benchmarks/apache-spark/src/main/scala/org/renaissance/apache/spark/PageRank.scala
+++ benchmarks/apache-spark/src/main/scala/org/renaissance/apache/spark/PageRank.scala
@@ -1,18 +1,19 @@
 package org.renaissance.apache.spark
 
-import java.nio.charset.StandardCharsets
 import java.nio.file.Path
-import java.nio.file.Paths
+import java.util.regex.Pattern
 
-import org.apache.commons.io.FileUtils
 import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
 import org.renaissance.Benchmark
 import org.renaissance.Benchmark._
 import org.renaissance.BenchmarkContext
 import org.renaissance.BenchmarkResult
+import org.renaissance.BenchmarkResult.Assert
 import org.renaissance.BenchmarkResult.Validators
 import org.renaissance.License
+
+import scala.collection.JavaConverters._
 
 @Name("page-rank")
 @Group("apache-spark")
@@ -24,14 +25,19 @@ import org.renaissance.License
   Array(
     new Parameter(name = "thread_count", defaultValue = "$cpu.count"),
     new Parameter(name = "input_line_count", defaultValue = "-1"),
-    new Parameter(name = "expected_rank_count", defaultValue = "598652")
+    new Parameter(name = "expected_rank_count", defaultValue = "598652"),
+    new Parameter(name = "expected_rank_hash", defaultValue = "d8bf7171feef1b87")
   )
 )
 @Configurations(
   Array(
     new Configuration(
       name = "test",
-      settings = Array("input_line_count = 5000", "expected_rank_count = 1661")
+      settings = Array(
+        "input_line_count = 5000",
+        "expected_rank_count = 1661",
+        "expected_rank_hash=7ca80582125b8ca7"
+      )
     ),
     new Configuration(name = "jmh")
   )
@@ -47,65 +53,63 @@ final class PageRank extends Benchmark with SparkUtil {
 
   private var expectedRankCountParam: Int = _
 
+  private var expectedRankHashParam: String = _
+
   private val ITERATIONS = 2
 
   // TODO: Unify handling of scratch directories throughout the suite.
   //  See: https://github.com/renaissance-benchmarks/renaissance/issues/13
 
-  val pageRankPath = Paths.get("target", "page-rank")
-
-  val outputPath = pageRankPath.resolve("output")
-
   val inputZipFile = "web-berkstan.txt.zip"
-
-  val bigInputFile = pageRankPath.resolve("bigfile.txt")
+  val inputFile = "web-BerkStan.txt"
 
   var sc: SparkContext = _
 
   var links: RDD[(String, Iterable[String])] = _
 
-  var ranks: RDD[(String, Double)] = _
-
   var tempDirPath: Path = _
 
-  def prepareInput(c: BenchmarkContext) = {
-    FileUtils.deleteDirectory(pageRankPath.toFile)
-    var inputText = ZipResourceUtil.readZipFromResourceToText(inputZipFile)
-    if (inputLineCountParam >= 0) {
-      inputText = inputText.linesWithSeparators.take(inputLineCountParam).mkString
-    }
-
-    FileUtils.write(bigInputFile.toFile, inputText, StandardCharsets.UTF_8, true)
-  }
-
-  def loadData() = {
-    val lines = sc.textFile(bigInputFile.toString)
-    links = lines
-      .map { line =>
-        val parts = line.split("\\s+")
-        (parts(0), parts(1))
+  def loadData(
+    zipName: String,
+    entryName: String,
+    lineCount: Int
+  ): RDD[(String, Iterable[String])] = {
+    val inputSource = ZipResourceUtil.sourceFromZipResource(entryName, zipName)
+    try {
+      var inputLines = inputSource.getLines()
+      if (lineCount >= 0) {
+        inputLines = inputLines.take(lineCount)
       }
-      .distinct()
-      .groupByKey()
-      .cache()
 
-//    ranks = links.mapValues(v => 1.0)
+      val splitter = Pattern.compile("\\s+")
+      sc.parallelize(inputLines.toSeq)
+        .map { line =>
+          val parts = splitter.split(line)
+          parts(0) -> parts(1)
+        }
+        .distinct()
+        .groupByKey()
+        .cache()
+    } finally {
+      inputSource.close()
+    }
   }
 
   override def setUpBeforeAll(c: BenchmarkContext): Unit = {
     threadCountParam = c.intParameter("thread_count")
-    expectedRankCountParam = c.intParameter("expected_rank_count")
     inputLineCountParam = c.intParameter("input_line_count")
+    expectedRankCountParam = c.intParameter("expected_rank_count")
+    expectedRankHashParam = c.stringParameter("expected_rank_hash")
 
     tempDirPath = c.generateTempDir("page_rank")
     sc = setUpSparkContext(tempDirPath, threadCountParam, "page-rank")
-    prepareInput(c)
-    loadData()
+    links = loadData(inputZipFile, inputFile, inputLineCountParam)
   }
 
   override def run(c: BenchmarkContext): BenchmarkResult = {
-    ranks = links.mapValues(v => 1.0)
-    for (i <- 0 until ITERATIONS) {
+    var ranks = links.mapValues(_ => 1.0)
+
+    for (_ <- 0 until ITERATIONS) {
       val contributions = links.join(ranks).values.flatMap {
         case (urls, rank) =>
           urls.map(url => (url, rank / urls.size))
@@ -113,19 +117,39 @@ final class PageRank extends Benchmark with SparkUtil {
       ranks = contributions.reduceByKey(_ + _).mapValues(0.15 + 0.85 * _)
     }
 
-    // TODO: add more sophisticated validation
-    Validators.simple("ranks count", expectedRankCountParam, ranks.count())
+    //
+    // Trigger the computation by counting the ranks. An alternative is to
+    // call collect(), which would also gather all the data into the driver
+    // as part of the measured operation. A typical application would probably
+    // iterate over a (potentially large) result (which is what count() will
+    // do) instead of collecting it into one place.
+    //
+    val rankCount: Int = ranks.count().toInt
+
+    new BenchmarkResult {
+      //
+      // Validate the result. Check the number of ranks first, and then the
+      // hash of the string representation of the ranks. Round the ranks to
+      // 8 decimal places and order them by the web site id to avoid numerical
+      // instability issues on different platforms.
+      //
+      override def validate() = {
+        Assert.assertEquals(expectedRankCountParam, rankCount, "ranks count")
+
+        val rankLines = ranks
+          .sortByKey()
+          .map {
+            case (url, rank) => f"$url $rank%.8f"
+          }
+          .collect
+          .toList
+
+        Validators.hashing(expectedRankHashParam, rankLines.asJava).validate()
+      }
+    }
   }
 
   override def tearDownAfterAll(c: BenchmarkContext): Unit = {
-    val output = ranks
-      .collect()
-      .map {
-        case (url, rank) => s"$url $rank"
-      }
-      .mkString("\n")
-    FileUtils.write(outputPath.toFile, output, StandardCharsets.UTF_8, true)
-
     tearDownSparkContext(sc)
     c.deleteTempDir(tempDirPath)
   }
--- benchmarks/apache-spark/src/main/scala/org/renaissance/apache/spark/ZipResourceUtil.scala
+++ benchmarks/apache-spark/src/main/scala/org/renaissance/apache/spark/ZipResourceUtil.scala
@@ -1,21 +1,40 @@
 package org.renaissance.apache.spark
 
+import java.io.FileNotFoundException
 import java.nio.charset.StandardCharsets
 import java.util.zip.ZipInputStream
 
-import org.apache.commons.io.IOUtils
+import scala.io.BufferedSource
+import scala.io.Source
 
 object ZipResourceUtil {
 
-  def readZipFromResourceToText(resourceName: String): String = {
-    val zis = new ZipInputStream(this.getClass.getResourceAsStream("/" + resourceName))
+  def sourceFromZipResource(entryName: String, resourceName: String): BufferedSource = {
+    val is = this.getClass.getResourceAsStream("/" + resourceName)
+    if (is == null) {
+      throw new FileNotFoundException(
+        s"resource '$resourceName' not found"
+      )
+    }
 
+    val zis = new ZipInputStream(is)
     try {
-      zis.getNextEntry()
-      IOUtils.toString(zis, StandardCharsets.UTF_8)
-
-    } finally {
-      zis.close()
+      Iterator
+        .continually(zis.getNextEntry)
+        .takeWhile(_ != null)
+        .filterNot(_.isDirectory)
+        .find(_.getName.equalsIgnoreCase(entryName))
+        .map(_ => Source.fromInputStream(zis, StandardCharsets.UTF_8.name))
+        .getOrElse {
+          throw new FileNotFoundException(
+            s"file '$entryName' not found in resource '$resourceName'"
+          )
+        }
+    } catch {
+      // Close the stream and propagate the exception
+      case e: Throwable =>
+        zis.close()
+        throw e
     }
   }
 
