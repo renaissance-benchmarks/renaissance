ca0ab03232902e570eb9097c30bfdc5dd388a702
Lubomír Bulej <bulej@d3s.mff.cuni.cz>
2019-08-31 16:48:07 +0200

    Hash web site id sequence ordered by (relaxed) ranks and ids
    Instead of rounding/truncating ranks and hashing the web site ids with
    the expected rank values, we now hash the sequence of web site ids
    ordered by their rank and their id.
    
    The ordering is relaxed by making neighboring ranks that are close
    together equivalent, thus creating an equivalence class within which
    the web sites are sorted by their id.
    
    This is slightly better than rounding/truncating ranks, because
    increasing the limit on difference between ranks should have a
    stabilizing effect, whereas rounding/truncating ranks remains
    fragile regardless of the decimal order chosen.

--- benchmarks/apache-spark/src/main/scala/org/renaissance/apache/spark/PageRank.scala
+++ benchmarks/apache-spark/src/main/scala/org/renaissance/apache/spark/PageRank.scala
@@ -26,7 +26,7 @@ import scala.collection.JavaConverters._
     new Parameter(name = "thread_count", defaultValue = "$cpu.count"),
     new Parameter(name = "input_line_count", defaultValue = "-1"),
     new Parameter(name = "expected_rank_count", defaultValue = "598652"),
-    new Parameter(name = "expected_rank_hash", defaultValue = "4704db55b9f60104")
+    new Parameter(name = "expected_rank_hash", defaultValue = "9b39ddf5eaa8b3d2")
   )
 )
 @Configurations(
@@ -35,8 +35,8 @@ import scala.collection.JavaConverters._
       name = "test",
       settings = Array(
         "input_line_count = 5000",
-        "expected_rank_count = 1661",
-        "expected_rank_hash = 9fd2e94dca375b4e"
+        "expected_rank_count = 1664",
+        "expected_rank_hash = 797021674f62a217"
       )
     ),
     new Configuration(name = "jmh")
@@ -55,28 +55,34 @@ final class PageRank extends Benchmark with SparkUtil {
 
   private var expectedRankHashParam: String = _
 
-  private val ITERATIONS = 2
+  /** Number of iterations of the page rank algorithm. */
+  private val PAGE_RANK_ITERATIONS = 2
+
+  private val INPUT_ZIP_RESOURCE = "/web-berkstan.txt.zip"
+
+  private val INPUT_ZIP_ENTRY = "web-BerkStan.txt"
+
+  /**
+   * Maximum difference in neighboring ranks for web sites in the same class.
+   * The value is slightly lower but in the same order as 1/685230, which
+   * corresponds to the number of web sites in the input data set
+   */
+  private val RANK_DIFFERENCE_LIMIT = 1e-6
 
   // TODO: Unify handling of scratch directories throughout the suite.
   //  See: https://github.com/renaissance-benchmarks/renaissance/issues/13
 
-  val inputZipResource = "/web-berkstan.txt.zip"
-  val inputFile = "web-BerkStan.txt"
+  private var sc: SparkContext = _
 
-  var sc: SparkContext = _
+  private var links: RDD[(Int, Iterable[Int])] = _
 
-  var links: RDD[(String, Iterable[String])] = _
+  private var tempDirPath: Path = _
 
-  var tempDirPath: Path = _
-
-  def loadData(
-    zipName: String,
-    entryName: String,
-    lineCount: Int
-  ): RDD[(String, Iterable[String])] = {
+  private def loadData(zipName: String, entryName: String, lineCount: Int) = {
     val inputSource = ZipResourceUtil.sourceFromZipResource(entryName, zipName)
+
     try {
-      var inputLines = inputSource.getLines()
+      var inputLines = inputSource.getLines().dropWhile(_.startsWith("#"))
       if (lineCount >= 0) {
         inputLines = inputLines.take(lineCount)
       }
@@ -84,10 +90,9 @@ final class PageRank extends Benchmark with SparkUtil {
       val splitter = Pattern.compile("\\s+")
       sc.parallelize(inputLines.toSeq)
         .map { line =>
-          val parts = splitter.split(line)
-          parts(0) -> parts(1)
+          val parts = splitter.split(line, 2)
+          parts(0).toInt -> parts(1).toInt
         }
-        .distinct()
         .groupByKey()
         .cache()
     } finally {
@@ -103,16 +108,15 @@ final class PageRank extends Benchmark with SparkUtil {
 
     tempDirPath = c.generateTempDir("page_rank")
     sc = setUpSparkContext(tempDirPath, threadCountParam, "page-rank")
-    links = loadData(inputZipResource, inputFile, inputLineCountParam)
+    links = loadData(INPUT_ZIP_RESOURCE, INPUT_ZIP_ENTRY, inputLineCountParam)
+
   }
 
   override def run(c: BenchmarkContext): BenchmarkResult = {
     var ranks = links.mapValues(_ => 1.0)
-
-    for (_ <- 0 until ITERATIONS) {
+    for (_ <- 0 until PAGE_RANK_ITERATIONS) {
       val contributions = links.join(ranks).values.flatMap {
-        case (urls, rank) =>
-          urls.map(url => (url, rank / urls.size))
+        case (urls, rank) => urls.map(url => (url, rank / urls.size))
       }
       ranks = contributions.reduceByKey(_ + _).mapValues(0.15 + 0.85 * _)
     }
@@ -121,31 +125,48 @@ final class PageRank extends Benchmark with SparkUtil {
     // Trigger the computation by counting the ranks. An alternative is to
     // call collect(), which would also gather all the data into the driver
     // as part of the measured operation. A typical application would probably
-    // iterate over a (potentially large) result (which is what count() will
+    // iterate over a (potentially large) result (which is what count() may
     // do) instead of collecting it into one place.
     //
     val rankCount: Int = ranks.count().toInt
 
     new BenchmarkResult {
       //
-      // Validate the result. Check the number of ranks first, and then the
-      // hash of the string representation of the ranks. Round the ranks to
-      // 8 decimal places and order them by the web site id to avoid numerical
-      // instability issues on different platforms.
+      // Check the number of ranks and the string representation of the web site
+      // identifiers ordered by rank and web site id (for equivalent ranks). To
+      // work around numerical instability issues, the sites are first pre-sorted
+      // by rank, and the ordering is then relaxed so that neighboring ranks that
+      // differ only slightly are considered equal.
       //
       override def validate() = {
         Assert.assertEquals(expectedRankCountParam, rankCount, "ranks count")
 
-        val rankLines = ranks
-          .sortByKey()
-          .collect
-          .map {
-            case (url, rank) => f"$url $rank%.7f"
-          }
-          .toList
+        val preSortedEntries = ranks.sortBy { case (_, rank) => rank }.collect
+        val sortedEntries = relaxedRanks(preSortedEntries, RANK_DIFFERENCE_LIMIT).sortBy {
+          case (url, rank) => (rank, url)
+        }
 
+        val rankLines = sortedEntries.map { case (url, _) => url.toString }.toList
         Validators.hashing(expectedRankHashParam, rankLines.asJava).validate()
       }
+
+      private def relaxedRanks(entries: Seq[(Int, Double)], diffLimit: Double) = {
+        var prevRank = entries(0)._2
+        var groupRank = prevRank
+
+        entries
+          .map {
+            case (url, rank) =>
+              val diff = rank - prevRank
+              if (diff > diffLimit) {
+                groupRank = rank
+              }
+
+              prevRank = rank
+              (url, groupRank)
+          }
+      }
+
     }
   }
 
