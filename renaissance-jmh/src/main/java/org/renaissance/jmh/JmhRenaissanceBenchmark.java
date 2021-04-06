86d4f9f5e550c7b0ea9c17e80073f084dd6e6eb4
Lubomír Bulej <bulej@d3s.mff.cuni.cz>
2021-04-06 09:26:21 +0200

    Update JMH wrapper to implement scratchDirectory() in BenchmarkContext

--- renaissance-jmh/src/main/java/org/renaissance/jmh/JmhRenaissanceBenchmark.java
+++ renaissance-jmh/src/main/java/org/renaissance/jmh/JmhRenaissanceBenchmark.java
@@ -1,24 +1,38 @@
 package org.renaissance.jmh;
 
-import org.openjdk.jmh.annotations.*;
-import org.renaissance.Benchmark;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.TearDown;
 import org.renaissance.BenchmarkContext;
 import org.renaissance.BenchmarkResult;
 import org.renaissance.BenchmarkResult.ValidationException;
 import org.renaissance.core.BenchmarkInfo;
 import org.renaissance.core.BenchmarkRegistry;
+import org.renaissance.core.DirUtils;
+
+import java.io.IOException;
+import java.nio.file.Path;
 
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 
 public abstract class JmhRenaissanceBenchmark {
-  private final Benchmark benchmark;
+  private final org.renaissance.Benchmark benchmark;
   private final BenchmarkContext context;
+  private final Path scratchBase;
+
   private BenchmarkResult result;
+  private Path scratchDir;
 
   protected JmhRenaissanceBenchmark(final String name) {
     BenchmarkInfo benchInfo = BenchmarkRegistry.createDefault().get(name);
     benchmark = BenchmarkRegistry.loadBenchmark(benchInfo);
     context = createBenchmarkContext(benchInfo);
+    scratchBase = Path.of(".");
   }
 
   @Setup(Level.Trial)
@@ -31,7 +45,7 @@ public abstract class JmhRenaissanceBenchmark {
     benchmark.setUpBeforeEach(context);
   }
 
-  @org.openjdk.jmh.annotations.Benchmark
+  @Benchmark
   @BenchmarkMode(Mode.SingleShotTime)
   @OutputTimeUnit(MILLISECONDS)
   @Measurement(timeUnit = MILLISECONDS)
@@ -56,6 +70,7 @@ public abstract class JmhRenaissanceBenchmark {
 
   private BenchmarkContext createBenchmarkContext(BenchmarkInfo benchInfo) {
     return new BenchmarkContext() {
+
       @Override
       public int intParameter(String name) {
         return Integer.parseInt(stringParameter(name));
@@ -73,6 +88,20 @@ public abstract class JmhRenaissanceBenchmark {
         return benchInfo.parameter("jmh", name);
       }
 
+      @Override
+      public Path scratchDirectory() {
+        if (scratchDir == null) {
+          try {
+            final String prefix = "jmh-"+ benchInfo.name() +"-";
+            scratchDir = DirUtils.createScratchDirectory(scratchBase, prefix, false);
+          } catch (IOException e) {
+            // This is a problem, fail the benchmark.
+            throw new RuntimeException("failed to create benchmark scratch directory", e);
+          }
+        }
+
+        return scratchDir;
+      }
     };
   }
 
