9dc9ae5beef202fdb8a3c5ab90091f0624dc78c7
Lubomír Bulej <bulej@d3s.mff.cuni.cz>
2021-05-10 09:41:15 +0200

    Add a script to run JMH bundle

--- tools/renaissance-jmh.sh
+++ tools/renaissance-jmh.sh
@@ -5,4 +5,4 @@ ROOT_DIR="$(git -C "$BASE_DIR" rev-parse --show-toplevel)"
 RENAISSANCE_GIT_VERSION=$(git -C "$BASE_DIR" describe --dirty=-SNAPSHOT)
 RENAISSANCE_VERSION=${RENAISSANCE_GIT_VERSION#v}
 
-exec java $JAVA_OPTS -jar "$ROOT_DIR/target/renaissance-gpl-$RENAISSANCE_VERSION.jar" "$@"
+exec java $JAVA_OPTS -jar "$ROOT_DIR/renaissance-jmh/target/scala-2.12/renaissance-jmh-assembly-$RENAISSANCE_VERSION.jar" "$@"
