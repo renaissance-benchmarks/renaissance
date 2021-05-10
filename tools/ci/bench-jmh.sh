f0317902e52f9460412d6971b1d8f5af14abd28f
Lubomír Bulej <bulej@d3s.mff.cuni.cz>
2021-05-10 09:40:27 +0200

    Use `test` configuration in CI runs of JMH bundle

--- tools/ci/bench-jmh.sh
+++ tools/ci/bench-jmh.sh
@@ -2,4 +2,7 @@
 source "$(dirname "$0")/common.sh"
 
 # Test benchmarks under JMH harness, replacing incompatible benchmarks with 'dummy-empty'
-java -Xms2500M -Xmx2500M -Dorg.renaissance.jmh.fakeIncompatible=true -jar "$RENAISSANCE_JMH_JAR" -wi 0 -i 1 -f 1 -foe true
+java -Xms2500M -Xmx2500M \
+	-Dorg.renaissance.jmh.fakeIncompatible=true \
+	-Dorg.renaissance.jmh.configuration=test \
+	-jar "$RENAISSANCE_JMH_JAR" -wi 0 -i 1 -f 1 -foe true
