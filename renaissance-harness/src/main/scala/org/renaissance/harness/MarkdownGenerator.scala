a924e95d90a6972ab369c7c36aa73e5dcb6c8105
Vojtech Horky <vojtech.horky@gmail.com>
2021-08-12 10:14:34 +0200

    Separate devel doc for plugins

--- renaissance-harness/src/main/scala/org/renaissance/harness/MarkdownGenerator.scala
+++ renaissance-harness/src/main/scala/org/renaissance/harness/MarkdownGenerator.scala
@@ -304,76 +304,26 @@ you will need to be able to tell the harness when to keep executing the benchmar
 stop.
 
 To this end, the suite provides hooks for plugins which can subscribe to events related to
-harness state and benchmark execution. A plugin is a user-defined class which must implement
-the `${tags("pluginClass")}` marker interface and provide at least a default (parameter-less)
-constructor. However, such a minimal plugin would not receive any notifications. To receive
-notifications, the plugin class must implement interfaces from the `${tags("pluginClass")}`
-interface name space depending on the type of events it wants to receive, or services it wants
-to provide. This is demonstrated in the following example:
+harness state and benchmark execution.
 
-```scala
-class SimplePlugin extends ${tags("pluginClass")}
-  with ${tags("afterHarnessInitListenerClass")}
-  with ${tags("afterOperationSetUpListenerClass")}
-  with ${tags("beforeOperationTearDownListenerClass")} {
+This repository contains two such plugins: one that uses a native agent built with
+[PAPI](https://icl.utk.edu/papi/) to collect information from hardware counters and
+a plugin for collecting information from a
+[CompilationMXBean](https://docs.oracle.com/javase/8/docs/api/java/lang/management/CompilationMXBean.html).
 
-  override def afterHarnessInit() = {
-    // Initialize the plugin after the harness finished initializing
-  }
-
-  override def afterOperationSetUp(benchmark: String, index: Int) = {
-    // Notify the tool that the measured operation is about to start.
-  }
-
-  override def beforeOperationTearDown(benchmark: String, index: Int) = {
-    // Notify the tool that the measured operations has finished.
-  }
-}
-```
-
-The following interfaces provide common (paired) event types which allow a plugin to hook
-into a specific point in the benchmark execution sequence. They are analogous to common
-annotations known from testing frameworks such as JUnit. Harness-level events occur only
-once per the whole execution, benchmark-level events occur once for each benchmark
-executed, and operation-level events occur once for each execution of the measured
-operation.
-- `${tags("afterHarnessInitListenerClass")}`, `${tags("beforeHarnessShutdownListenerClass")}`
-- `${tags("beforeBenchmarkSetUpListenerClass")}`, `${tags("afterBenchmarkTearDownListenerClass")}`
-- `${tags("afterBenchmarkSetUpListenerClass")}`, `${tags("beforeBenchmarkTearDownListenerClass")}`
-- `${tags("afterOperationSetUpListenerClass")}`, `${tags("beforeOperationTearDownListenerClass")}`
-
-The following interfaces provide special non-paired event types:
-- `${tags("measurementResultListenerClass")}`, intended for plugins that want to receive
-measurements results (perhaps to store them in a custom format). The harness calls the
-`onMeasurementResult` method with the name of the metric and its value, but only if the
-benchmark operation produces a valid result.
-- `${tags("benchmarkFailureListenerClass")}`, which indicates that the benchmark execution
-has either failed in some way (the benchmark triggered an exception), or that the benchmark
-operation produced a result which failed validation. This means that no measurements results
-will be received.
-
-And finally the following interfaces are used by the harness to request
-services from plugins:
-- `${tags("measurementResultPublisherClass")}`, intended for plugins that want to collect
-values of additional metrics around the execution of the benchmark operation. The harness
-calls the `onMeasurementResultsRequested` method with an instance of event dispatcher which
-the plugin is supposed to use to notify other result listeners about custom measurement results.
-- `${tags("policyClass")}`, intended for plugins that want to control the execution
-of the benchmark's measured operation. Such a plugin should implement other interfaces to
-get enough information to determine, per-benchmark, whether to execute the measured operation
-or not. The harness calls the `canExecute` method before executing the benchmark's measured
-operation, and will pass the result of the `isLast` method to some other events.
+If you wish to create your own plugin, please consult
+[documentation/plugins.md](documentation/plugins.md) for more details.
 
 To make the harness use an external plugin, it needs to be specified on the command line.
 The harness can load multiple plugins, and each must be enabled using the
 `--plugin <class-path>[!<class-name>]` option. The `<class-path>` is the class path on which
-to look for the plugin class, and `<class-name>` is a fully qualified name of the plugin class
-(the class-name can be omitted when specified as a `Renaissance-Plugin` property inside
-`META-INF/MANIFEST.MF` of the plugin JAR).
+to look for the plugin class (optionally, you may add `<class-name>` to specify a fully
+qualified name of the plugin class).
+
 Custom execution policy must be enabled using the `--policy <class-path>!<class-name>` option.
 The syntax is the same as in case of normal plugins (and the policy is also a plugin, which
 can register for all event types), but this option tells the harness to actually use the
-plugin to control benchmark execution. Other than that, policy is treated the same was as
+plugin to control benchmark execution. Other than that, policy is treated the same way as a
 plugin.
 
 When registering plugins for pair events (harness init/shutdown, benchmark set up/tear down,
@@ -387,10 +337,7 @@ Plugins (and policies) can receive additional command line arguments. Each argum
 given using the `--with-arg <arg>` option, which appends `<arg>` to the list of arguments for
 the plugin (or policy) that was last mentioned on the command line. Whenever a `--plugin`
 (or `--policy`) option is encountered, the subsequent `--with-arg` options will append
-arguments to that plugin (or policy). A plugin that wants to receive command line arguments
-must define a constructor which takes an array of strings (`String[]`) or a string vararg
-(`String...`) as parameter. The harness tries to use this constructor first and falls back
-to the default (parameter-less) constructor.
+arguments to that plugin (or policy).
 
 
 ### JMH support
