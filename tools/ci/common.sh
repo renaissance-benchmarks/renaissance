1d1db52c4a8ebae399026dc7a8ab054f21118efb
Vojtech Horky <vojtech.horky@gmail.com>
2023-04-12 13:39:03 +0200

    CI: run sbt with --debug

--- tools/ci/common.sh
+++ tools/ci/common.sh
@@ -42,7 +42,7 @@ RENAISSANCE_JMH_JAR="${RENAISSANCE_JMH_DIR}/$RENAISSANCE_JMH_JAR_NAME"
 
 ci_sbt() {
 	local TRUST_STORE="$ROOT_DIR/tools/jks/cacerts"
-	$ROOT_DIR/tools/sbt/bin/sbt "-J-Djavax.net.ssl.trustStore=$TRUST_STORE" "$@"
+	$ROOT_DIR/tools/sbt/bin/sbt --debug "-J-Djavax.net.ssl.trustStore=$TRUST_STORE" "$@"
 }
 
 cp_reflink() {
