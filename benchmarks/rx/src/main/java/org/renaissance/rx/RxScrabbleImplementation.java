16abdcf6b9f3d44814f5c746e0684c54d2593714
Lubomír Bulej <bulej@d3s.mff.cuni.cz>
2023-10-05 00:24:29 +0200

    Use Single for single-value observables
    The Single type better fits the actual usage.

--- benchmarks/rx/src/main/java/org/renaissance/rx/RxScrabbleImplementation.java
+++ benchmarks/rx/src/main/java/org/renaissance/rx/RxScrabbleImplementation.java
@@ -20,6 +20,7 @@ package org.renaissance.rx;
 
 
 import rx.Observable;
+import rx.Single;
 import rx.functions.Func1;
 import rx.schedulers.Schedulers;
 
@@ -40,13 +41,13 @@ public class RxScrabbleImplementation extends Scrabble {
 
   public List<Entry<Integer, List<String>>> runScrabble() throws InterruptedException {
     // Function to compute the score of a given word
-    Func1<Integer, Observable<Integer>> scoreOfALetter =
-      letter -> Observable.just(letterScores[letter - 'a']);
+    Func1<Integer, Single<Integer>> scoreOfALetter =
+      letter -> Single.just(letterScores[letter - 'a']);
 
     // score of the same letters in a word
-    Func1<Entry<Integer, LongWrapper>, Observable<Integer>> letterScore =
+    Func1<Entry<Integer, LongWrapper>, Single<Integer>> letterScore =
       entry ->
-        Observable.just(
+        Single.just(
           letterScores[entry.getKey() - 'a'] *
             Integer.min(
               (int) entry.getValue().get(),
@@ -58,18 +59,18 @@ public class RxScrabbleImplementation extends Scrabble {
       string -> Observable.from(() -> string.chars().boxed().iterator());
 
     // Histogram of the letters in a given word
-    Func1<String, Observable<HashMap<Integer, LongWrapper>>> histoOfLetters =
+    Func1<String, Single<HashMap<Integer, LongWrapper>>> histoOfLetters =
       word -> toIntegerObservable.call(word)
         .collect(
           HashMap::new,
           (HashMap<Integer, LongWrapper> map, Integer value) ->
             map.merge(value, () -> 0L, (prev, cur) -> prev.inc())
-        );
+        ).toSingle();
 
     // number of blanks for a given letter
-    Func1<Entry<Integer, LongWrapper>, Observable<Long>> blank =
+    Func1<Entry<Integer, LongWrapper>, Single<Long>> blank =
       entry ->
-        Observable.just(
+        Single.just(
           Long.max(
             0L,
             entry.getValue().get() -
@@ -78,24 +79,24 @@ public class RxScrabbleImplementation extends Scrabble {
         );
 
     // number of blanks for a given word
-    Func1<String, Observable<Long>> nBlanks =
+    Func1<String, Single<Long>> nBlanks =
       word -> histoOfLetters.call(word)
-        .flatMap(map -> Observable.from(map.entrySet()))
-        .flatMap(blank)
-        .reduce(Long::sum);
+        .flatMapObservable(map -> Observable.from(map.entrySet()))
+        .flatMapSingle(blank)
+        .reduce(Long::sum).toSingle();
 
 
     // can a word be written with 2 blanks?
-    Func1<String, Observable<Boolean>> checkBlanks =
+    Func1<String, Single<Boolean>> checkBlanks =
       word -> nBlanks.call(word)
-        .flatMap(l -> Observable.just(l <= 2L));
+        .flatMap(l -> Single.just(l <= 2L));
 
     // score taking blanks into account letterScore1
-    Func1<String, Observable<Integer>> score2 =
+    Func1<String, Single<Integer>> score2 =
       word -> histoOfLetters.call(word)
-        .flatMap(map -> Observable.from(map.entrySet()))
-        .flatMap(letterScore)
-        .reduce(Integer::sum);
+        .flatMapObservable(map -> Observable.from(map.entrySet()))
+        .flatMapSingle(letterScore)
+        .reduce(Integer::sum).toSingle();
 
     // Placing the word on the board
     // Building the streams of first and last letters
@@ -110,25 +111,25 @@ public class RxScrabbleImplementation extends Scrabble {
       word -> Observable.concat(first3.call(word), last3.call(word));
 
     // Bonus for double letter
-    Func1<String, Observable<Integer>> bonusForDoubleLetter =
+    Func1<String, Single<Integer>> bonusForDoubleLetter =
       word -> toBeMaxed.call(word)
-        .flatMap(scoreOfALetter)
-        .reduce(Integer::max);
+        .flatMapSingle(scoreOfALetter)
+        .reduce(Integer::max).toSingle();
 
     // score of the word put on the board
-    Func1<String, Observable<Integer>> score3 =
+    Func1<String, Single<Integer>> score3 =
       word ->
-        Observable.concat(
+        Single.concat(
           score2.call(word),
           score2.call(word),
           bonusForDoubleLetter.call(word),
           bonusForDoubleLetter.call(word),
-          Observable.just(word.length() == 7 ? 50 : 0)
+          Single.just(word.length() == 7 ? 50 : 0)
         )
-        .reduce(Integer::sum);
+        .reduce(Integer::sum).toSingle();
 
     Func1<
-      Func1<String, Observable<Integer>>, Observable<TreeMap<Integer, List<String>>>
+      Func1<String, Single<Integer>>, Single<TreeMap<Integer, List<String>>>
       > buildHistoOnScore =
       score -> Observable.from(shakespeareWords)
         .buffer(1024)
@@ -136,20 +137,20 @@ public class RxScrabbleImplementation extends Scrabble {
           Observable.from(buffer)
             .subscribeOn(Schedulers.computation())
             .filter(scrabbleWords::contains)
-            .filter(word -> checkBlanks.call(word).toBlocking().first())
+            .filter(word -> checkBlanks.call(word).toBlocking().value())
         )
         .collect(
           () -> new TreeMap<>(Comparator.reverseOrder()),
           (TreeMap<Integer, List<String>> map, String word) -> {
-            Integer key = score.call(word).toBlocking().first();
+            Integer key = score.call(word).toBlocking().value();
             List<String> list = map.computeIfAbsent(key, k -> new ArrayList<>());
             list.add(word);
-          });
+          }).toSingle();
 
     // best key / value pairs
     List<Entry<Integer, List<String>>> finalList2 =
       buildHistoOnScore.call(score3)
-        .flatMap(map -> Observable.from(map.entrySet()))
+        .flatMapObservable(map -> Observable.from(map.entrySet()))
         .collect(
           () -> new ArrayList<Entry<Integer, List<String>>>(),
           ArrayList::add
