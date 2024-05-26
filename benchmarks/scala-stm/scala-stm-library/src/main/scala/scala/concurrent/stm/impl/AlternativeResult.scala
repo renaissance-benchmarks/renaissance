cdb788f0a99835bbd1430d23461c71b23a5367b4
Lubomír Bulej <bulej@d3s.mff.cuni.cz>
2024-05-26 15:57:04 +0200

    Remove the scala-stm-library subproject
    This removes both the scala-stm library sources, the (unused,
    uncompilable) stmbench7 sources, and the stmbench7-VELOX and deuceAgent
    jars.

--- benchmarks/scala-stm/scala-stm-library/src/main/scala/scala/concurrent/stm/impl/AlternativeResult.scala
+++
@@ -1,9 +0,0 @@
-/* scala-stm - (c) 2009-2011, Stanford University, PPL */
-
-package scala.concurrent.stm
-package impl
-
-import util.control.ControlThrowable
-
-/** See `PendingAtomicBlock` */
-private[stm] case class AlternativeResult(value: Any) extends ControlThrowable
