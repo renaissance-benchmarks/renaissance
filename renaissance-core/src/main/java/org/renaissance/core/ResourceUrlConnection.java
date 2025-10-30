b5dd277c6743dff14e2cf47dc511ef820c7d1437
Lubomír Bulej <bulej@d3s.mff.cuni.cz>
2025-10-30 17:39:02 +0100

    Drop caching from ResourceUrlConnection
    The JAR files will be stored uncompressed (in the main bundle), so there
    is not much CPU work to be saved, and the operating system will cache
    the physical I/O. The extra memory consumption is not worth it.

--- renaissance-core/src/main/java/org/renaissance/core/ResourceUrlConnection.java
+++ renaissance-core/src/main/java/org/renaissance/core/ResourceUrlConnection.java
@@ -1,23 +1,18 @@
 package org.renaissance.core;
 
-import java.io.ByteArrayInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.lang.ref.WeakReference;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLStreamHandler;
 import java.net.URLStreamHandlerFactory;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
 
 /**
- * An implementation of a {@link URLConnection} for resources. Serves primarily
- * to allow loading classes from resource JAR files. Because a single resource
- * may be accessed simultaneously from multiple connections, this class caches
- * the resource contents so that it can be shared between active connections.
- * The cached content is released after all connections using it are closed.
+ * Minimalistic {@link URLConnection} to enable loading classes from resource
+ * JAR files. Multiple URL connections may be open simultaneously, but it does
+ * not pay off to cache the content. The JAR files will be stored uncompressed
+ * and the I/O to the main bundle will be cached by the operating system.
  */
 class ResourceUrlConnection extends URLConnection {
   static final String PROTOCOL = "resource";
@@ -28,34 +23,20 @@ class ResourceUrlConnection extends URLConnection {
     }
 
     return new URLStreamHandler() {
-      /** Cache shared by connections created by this handler. */
-      final ConcurrentMap<String, WeakReference<byte[]>> sharedCache = new ConcurrentHashMap<>();
-
       @Override
       protected URLConnection openConnection(URL url) throws IOException {
-        return new ResourceUrlConnection(url, sharedCache);
+        return new ResourceUrlConnection(url);
       }
     };
   };
 
   //
 
-  /**
-   * Content cache. The keys are @{link URL} paths (without anchor fragments),
-   * use strong references, and are never removed, even if the corresponding
-   * weak reference is cleared. An entry without content means that the
-   * resource actually exists and just needs to be loaded.
-   */
-  private final ConcurrentMap<String, WeakReference<byte[]>> weakContentByPath;
-
   /** A value of -1 indicates unknown length. */
   private int contentLength = -1;
 
-  ResourceUrlConnection(
-    URL url, ConcurrentMap<String, WeakReference<byte[]>> sharedCache
-  ) {
+  ResourceUrlConnection(URL url) {
     super(url);
-    weakContentByPath = sharedCache;
   }
 
   @Override
@@ -64,33 +45,12 @@ class ResourceUrlConnection extends URLConnection {
       return;
     }
 
-    final String urlPath = url.getPath();
-
-    // If the resource is still cached, fill the content length.
-    final WeakReference<byte[]> weakContent = weakContentByPath.get(urlPath);
-    if (weakContent != null) {
-      byte[] content = weakContent.get();
-      if (content != null) {
-        contentLength = content.length;
-        connected = true;
-        return;
-      }
-
-      // FALL THROUGH: The resource is not in the cache anymore.
-    }
-
-    //
-    // The resource is not in cache, determine its length but do not load it
-    // yet. We put a dummy entry in the cache so that we do not have to worry
-    // about it in the getInputStream() method.
-    //
-    try (InputStream resourceStream = getCoreResourceAsStream(urlPath)) {
+    // Determine content length but do not load it yet.
+    try (InputStream resourceStream = getCoreResourceAsStream(url.getPath())) {
       if (resourceStream == null) {
-        throw new FileNotFoundException(urlPath);
+        throw new FileNotFoundException(url.getPath());
       }
 
-      // Insert an entry marking that we have seen the resource.
-      weakContentByPath.putIfAbsent(urlPath, new WeakReference<>(null));
       contentLength = resourceStream.available();
       connected = true;
     }
@@ -104,23 +64,7 @@ class ResourceUrlConnection extends URLConnection {
     }
 
     assert connected;
-    final String urlPath = url.getPath();
-
-    // Connected implies that the cache has an entry, meaning
-    // the resource exists, but not that it is actually cached.
-    final byte[] cachedContent = weakContentByPath.get(urlPath).get();
-    if (cachedContent != null) {
-      return new ByteArrayInputStream(cachedContent);
-    }
-
-    // The content is not in the cache, load it.
-    try (InputStream resourceStream = getCoreResourceAsStream(urlPath)) {
-      assert resourceStream != null : "the resource stream is expected to exist";
-
-      final byte[] content = resourceStream.readAllBytes();
-      weakContentByPath.put(urlPath, new WeakReference<>(content));
-      return new ByteArrayInputStream(content);
-    }
+    return getCoreResourceAsStream(url.getPath());
   }
 
   private static InputStream getCoreResourceAsStream(String file) {
